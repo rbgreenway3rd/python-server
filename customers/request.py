71f354c5bdcefa896c046656aa7560ef1ddbe39d
rbgreenway3rd <3rdgreenway@gmail.com>
2021-07-13 15:53:51 -0500

    implemented delete functionality to all 4 modules

--- customers/request.py
+++ customers/request.py
@@ -64,3 +64,16 @@ def create_customer(customer):
     CUSTOMERS.append(customer)
 
     return customer
+
+
+def delete_customer(id):
+
+    customer_index = -1
+
+    for index, customer in enumerate(CUSTOMERS):
+        if customer["id"] == id:
+
+            customer_index = index
+
+    if customer_index >= 0:
+        CUSTOMERS.pop(customer_index)

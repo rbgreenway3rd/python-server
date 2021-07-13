f8c8bdcfeb3ff7e2d7b908eaf24e38d1f1e0cbd2
rbgreenway3rd <3rdgreenway@gmail.com>
2021-07-13 13:31:58 -0500

    first commit

---
+++ animals/request.py
@@ -0,0 +1,27 @@
+ANIMALS = [
+    {
+        "id": 1,
+        "name": "Winnie",
+        "species": "Dog",
+        "locationId": 1,
+        "customerId": 4
+    },
+    {
+        "id": 2,
+        "name": "Penny",
+        "species": "Dog",
+        "locationId": 1,
+        "customerId": 2
+    },
+    {
+        "id": 3,
+        "name": "Dinky",
+        "species": "Cat",
+        "locationId": 2,
+        "customerId": 1
+    }
+]
+
+
+def get_all_animals():
+    return ANIMALS
